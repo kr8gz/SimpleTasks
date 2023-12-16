dd027e9c38299cb651b77d2571245174b31bf02b
kr8gz <47776344+kr8gz@users.noreply.github.com>
2023-12-16 20:35:59 +0100

    fix test refactor accident

--- gradlew.bat
+++ gradlew.bat
@@ -84,7 +84,7 @@ rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
-exit /Subcommand %EXIT_CODE%
+exit /b %EXIT_CODE%
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
